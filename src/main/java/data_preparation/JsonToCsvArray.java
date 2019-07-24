b598c8b5cc718bfbc66679793fc9b558ae68793f
Karina <karinam6@interia.pl>
2019-07-24 20:19:32 +0200

    Initial commit

---
+++ src/main/java/data_preparation/JsonToCsvArray.java
@@ -0,0 +1,70 @@
+package data_preparation;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Array;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class JsonToCsvArray {
+
+    private static final String filePath = "src\\main\\resources\\statuses.json";
+    final static String[] CSV_HEADER = {"kontakt_id", "klient_id", "pracownik_id", "status", "kontakt_ts"};
+
+    public static void main(String[] args) {
+
+        FileWriter fileWriter = null;
+        CSVPrinter csvPrinter = null;
+        try {
+            FileReader reader = new FileReader(filePath);
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+            JSONArray records = (JSONArray) jsonObject.get("records");
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            fileWriter = new FileWriter("csv_output.csv");
+            csvPrinter = new CSVPrinter(fileWriter, CSVFormat.DEFAULT.withHeader(CSV_HEADER));
+            Iterator i = records.iterator();
+
+            SimpleDateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            Date date_filtr = inputDateFormat.parse("2017-07-01 00:00:00");
+            // Date date = inputDateFormat.parse(innerObj.get("kontakt_ts"));
+
+
+            while (i.hasNext()) {
+                JSONObject innerObj = (JSONObject) i.next();
+
+                Date date = inputDateFormat.parse(innerObj.get("kontakt_ts").toString());
+                if (date.after(date_filtr)) {
+
+                    csvPrinter.printRecord(innerObj.get("kontakt_id") + " | " + innerObj.get("klient_id") + " | " + innerObj.get("pracownik_id") + " | " + innerObj.get("status") + " | " + innerObj.get("kontakt_ts"));
+                }
+            }
+            // csvPrinter.println();
+        } catch (Exception e) {
+            System.out.println("Writing CSV error!");
+            e.printStackTrace();
+        } finally {
+            try {
+                fileWriter.flush();
+                fileWriter.close();
+                csvPrinter.close();
+            } catch (IOException e) {
+                System.out.println("Flushing/closing error!");
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+
+}
---
+++ src/main/java/data_preparation/JsonToCsvSimple.java
@@ -0,0 +1,69 @@
+package data_preparation;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonObjectFormatVisitor;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class JsonToCsvSimple {
+
+    private static final String filePath = "src\\main\\resources\\statuses.json";
+    final static String[] CSV_HEADER = {"kontakt_id", "klient_id", "pracownik_id", "status", "kontakt_ts"};
+
+    public static void main(String[] args) {
+
+        FileWriter fileWriter = null;
+        CSVPrinter csvPrinter = null;
+        try {
+            FileReader reader = new FileReader(filePath);
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+            JSONArray records = (JSONArray) jsonObject.get("records");
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            fileWriter = new FileWriter("csv_output.csv");
+            csvPrinter = new CSVPrinter(fileWriter, CSVFormat.DEFAULT.withHeader(CSV_HEADER));
+            Iterator i = records.iterator();
+
+            SimpleDateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            Date date_filtr = inputDateFormat.parse("2017-07-01 00:00:00");
+            // Date date = inputDateFormat.parse(innerObj.get("kontakt_ts"));
+
+            while (i.hasNext()) {
+                JSONObject innerObj = (JSONObject) i.next();
+
+                Date date = inputDateFormat.parse(innerObj.get("kontakt_ts").toString());
+                if (date.after(date_filtr)) {
+                    csvPrinter.printRecord(innerObj.get("kontakt_id") + " | " + innerObj.get("klient_id") + " | " + innerObj.get("pracownik_id") + " | " + innerObj.get("status") + " | " + innerObj.get("kontakt_ts"));
+                }
+            }
+            // csvPrinter.println();
+        } catch (Exception e) {
+            System.out.println("Writing CSV error!");
+            e.printStackTrace();
+        } finally {
+            try {
+                fileWriter.flush();
+                fileWriter.close();
+                csvPrinter.close();
+            } catch (IOException e) {
+                System.out.println("Flushing/closing error!");
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
